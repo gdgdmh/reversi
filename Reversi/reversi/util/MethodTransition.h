305b88a3c72f5aeb9ad3726687de561d1d932309
gdgdkuro <gdgdkuro@gmail.com>
2020-03-28 11:35:25 +0900

    クラス内メソッド遷移クラスを追加

---
+++ Reversi/reversi/util/MethodTransition.h
@@ -0,0 +1,39 @@
+﻿#ifndef REVERSI_UTIL_METHODTRANSITION_H_
+#define REVERSI_UTIL_METHODTRANSITION_H_
+
+namespace reversi {
+
+// クラス内メソッド遷移クラス
+template<typename T>
+class MethodTransition {
+public:
+	/**
+	 * コンストラクタ
+	 * @param execute_method 最初に実行するメソッドポインタ
+	 */
+	MethodTransition(MethodTransition<T>(T::*execute_method)()) : method(execute_method) {
+	};
+
+	/**
+	 * デストラクタ
+	 */
+	virtual ~MethodTransition() {
+	}
+
+	/**
+	 * シーン実行
+	 * @param  object 実行するクラスのthisポインタ
+	 * @return        メソッド遷移クラス
+	 */
+	MethodTransition<T> Execute(T* object) {
+		return (object->*scene)();
+	}
+
+private:
+	// 実行するクラスのメソッドポインタ
+	MethodTransition<T> (T::*method)();
+};
+
+}
+
+#endif  // REVERSI_UTIL_METHODTRANSITION_H_
