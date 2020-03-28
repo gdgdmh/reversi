7fc28f123ba241a6097c51e0d74ae53c065de262
gdgdkuro <gdgdkuro@gmail.com>
2020-03-28 22:55:17 +0900

    Assertクラスを追加

---
+++ Reversi/reversi/util/Assert.cpp
@@ -0,0 +1,34 @@
+﻿#include "Assert.h"
+#include "OutputConsole.h"
+
+/**
+ * 値の比較結果がfalseだったときにmessageを出力してプログラム停止する
+ * @param expected 比較結果
+ * @param message  出力するメッセージ
+ */
+void reversi::Assert::AssertEquals(bool expected, std::string message) {
+	if (expected == false) {
+		OutputConsole* output = new OutputConsole();
+		output->PrintLine(message);
+		if (output) {
+			delete output;
+			output = NULL;
+		}
+	}
+}
+
+/**
+ * 値の比較結果が0だったときにmessageを出力してプログラム停止する
+ * @param expected 比較結果
+ * @param message  出力するメッセージ
+ */
+void reversi::Assert::AssertEquals(int expected, std::string message) {
+	if (expected == 0) {
+		OutputConsole* output = new OutputConsole();
+		output->PrintLine(message);
+		if (output) {
+			delete output;
+			output = NULL;
+		}
+	}
+}
---
+++ Reversi/reversi/util/Assert.h
@@ -0,0 +1,30 @@
+﻿#ifndef REVERSI_SCENE_TESTSCENE_H_
+#define REVERSI_SCENE_TESTSCENE_H_
+
+#include <string>
+
+namespace reversi {
+
+// 実装テスト用クラス
+class Assert {
+public:
+
+	/**
+	 * 値の比較結果がfalseだったときにmessageを出力してプログラム停止する
+	 * @param expected 比較結果
+	 * @param message  出力するメッセージ
+	 */
+	static void AssertEquals(bool expected, std::string message);
+
+	/**
+	 * 値の比較結果が0だったときにmessageを出力してプログラム停止する
+	 * @param expected 比較結果
+	 * @param message  出力するメッセージ
+	 */
+	static void AssertEquals(int expected, std::string message);
+
+};
+
+}
+
+#endif  // REVERSI_SCENE_TESTSCENE_H_
