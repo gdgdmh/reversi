68a9099ddeb0c8ec5a46a3e8eb0d0a513ed53760
gdgdkuro <gdgdkuro@gmail.com>
2020-03-29 09:33:55 +0900

    盤の座標定義を追加

--- Reversi/reversi/logic/base/ReversiConstant.h
+++ Reversi/reversi/logic/base/ReversiConstant.h
@@ -17,6 +17,18 @@ public:
 	static const int BOARD_SIZE = 10 * 10;     // 盤の大きさ(1次元配列) 8x8の盤だが斜め判定を考慮して全体を無効マスで囲う用にしている
 	static const int BOARD_SIZE_X = 8;     // 盤の大きさX(Xマスの数)
 	static const int BOARD_SIZE_Y = 8;     // 盤の大きさY(Yマスの数)
+
+	//　盤の座標
+	enum POSITION {
+		A1, B1, C1, D1, E1, F1, G1, H1,
+		A2, B2, C2, D2, E2, F2, G2, H2,
+		A3, B3, C3, D3, E3, F3, G3, H3,
+		A4, B4, C4, D4, E4, F4, G4, H4,
+		A5, B5, C5, D5, E5, F5, G5, H5,
+		A6, B6, C6, D6, E6, F6, G6, H6,
+		A7, B7, C7, D7, E7, F7, G7, H7,
+		A8, B8, C8, D8, E8, F8, G8, H8
+	};
 };
 
 }
