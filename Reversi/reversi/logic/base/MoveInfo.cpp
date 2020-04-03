4f7c33451b0d51063a35128d5eb55dacd3d20983
gdgdkuro <gdgdkuro@gmail.com>
2020-04-03 16:15:18 +0900

    MoveInfoを追加

---
+++ Reversi/reversi/logic/base/MoveInfo.cpp
@@ -0,0 +1,16 @@
+﻿#include "MoveInfo.h"
+#include "../../util/Assert.h"
+
+
+/**
+ * コンストラクタ
+ * @param info 着手情報
+ */
+reversi::MoveInfo::MoveInfo(reversi::MoveInfo::MOVE_INFO info) : moveInfo(info) {
+}
+
+/**
+ * デストラクタ
+ */
+reversi::MoveInfo::~MoveInfo() {
+}
---
+++ Reversi/reversi/logic/base/MoveInfo.h
@@ -0,0 +1,48 @@
+﻿#ifndef REVERSI_LOGIC_BASE_MOVEINFO_H_
+#define REVERSI_LOGIC_BASE_MOVEINFO_H_
+
+#include "ReversiConstant.h"
+#include "ReverseInfo.h"
+
+namespace reversi {
+
+// 着手情報クラス
+class MoveInfo {
+public:
+
+	// 着手情報
+	typedef struct {
+		reversi::ReversiConstant::POSITION position;    // 位置
+		reversi::ReversiConstant::BOARD_INFO info;  // 着手する石
+		reversi::ReversiConstant::TURN turn;            // 手番
+		reversi::ReverseInfo::REVERSE_INFO reverseInfo; // 裏返し情報
+	} MOVE_INFO;
+
+public:
+
+	/**
+	 * コンストラクタ
+	 * @param info 着手情報
+	 */
+	MoveInfo(MOVE_INFO info);
+
+	/**
+	 * デストラクタ
+	 */
+	virtual ~MoveInfo();
+
+	/**
+	 * 着手情報取得
+	 * @return 着手情報
+	 */
+	MOVE_INFO Get() {
+		return moveInfo;
+	};
+
+private:
+	MOVE_INFO moveInfo;
+};
+
+}
+
+#endif  // REVERSI_LOGIC_BASE_MOVEINFO_H_
