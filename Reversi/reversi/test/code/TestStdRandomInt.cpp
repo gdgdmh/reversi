cf75a4a469d49a561292a0e021826459ff128b13
gdgdkuro <gdgdkuro@gmail.com>
2020-04-06 16:30:42 +0900

    テストコードにコメントを追記

--- Reversi/reversi/test/code/TestStdRandomInt.cpp
+++ Reversi/reversi/test/code/TestStdRandomInt.cpp
@@ -23,9 +23,9 @@ bool reversi::TestStdRandomInt::Execute() {
 
 	reversi::IRandomInt* random = new reversi::StdRandomInt();
 
-	const int INITIALIZE_SEED = 0;
-	const int MIN_RANDOM_VALUE = 0;
-	const int MAX_RANDOM_VALUE = 255;
+	const int INITIALIZE_SEED = 0; // 初期乱数SEED
+	const int MIN_RANDOM_VALUE = 0; // 範囲最低値
+	const int MAX_RANDOM_VALUE = 255; // 範囲最大値
 
 	// 同じ乱数の種を使うと同じ値が返ってくるはず
 	{
@@ -44,8 +44,9 @@ bool reversi::TestStdRandomInt::Execute() {
 	// 値の範囲に入っているかチェック(やりすぎても埒が明かないのでひとまず10回)
 	const int CHECK_COUNT = 10;
 	for (int i = 0; i < CHECK_COUNT; ++i) {
-		random->SetSeedByTime();
+		random->SetSeedByTime(); // 乱数の種は適当に生成
 		int result = random->Get(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+		// MINとMAXの間で生成されているかチェック
 		if (!AssertEqual((result >= MIN_RANDOM_VALUE) && (result <= MAX_RANDOM_VALUE), "TestStdRandomInt::Execute NOT VALUE RANGE")) {
 			if (random) {
 				delete random;
