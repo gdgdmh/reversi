519978d0477883e5d759bad35e2f76476e7d9e16
gdgdkuro <gdgdkuro@gmail.com>
2020-04-09 19:24:09 +0900

    新しい思考用のコードを追加

---
+++ Reversi/reversi/logic/player/MoveThinkingCpu3.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include "MoveThinkingCpu3.h"
+#include "../../util/IOutputConsole.h"
+#include "../../util/Assert.h"
+#include "../../util/IRandomInt.h"
+#include "../../util/StdRandomInt.h"
+
+// 定数定義
+// 静的評価位置
+const int reversi::MoveThinkingCpu3::STATIC_EVALUATION_POSITIONS[reversi::ReversiConstant::POSITION_SIZE] = {
+	 60,  -30,  20,   5,   5,  20, -30,  60,
+	-30,  -40,  -5,  -5,  -5,  -5, -40, -30,
+	 20,   -5,  20,   3,   3,  20,  -5,  20,
+	  5,   -5,   3,   3,   3,   3,  -5,   5,
+	  5,   -5,   3,   3,   3,   3,  -5,   5,
+	 20,   -5,  20,   3,   3,  20,  -5,  20,
+	-30,  -40,  -5,  -5,  -5,  -5, -40, -30,
+	 60,  -30,  20,   5,  20,  20, -30,  60,
+};
+// 参考元
+const int reversi::MoveThinkingCpu3::DEFAULT_STATIC_EVALUATION_POSITIONS[reversi::ReversiConstant::POSITION_SIZE] = {
+	120,  -20,  20,   5,   5,  20, -20, 120,
+	-20,  -40,  -5,  -5,  -5,  -5, -40, -20,
+	 20,   -5,  15,   3,   3,  15,  -5,  20,
+	  5,   -5,   3,   3,   3,   3,  -5,   5,
+	  5,   -5,   3,   3,   3,   3,  -5,   5,
+	 20,   -5,  15,   3,   3,  15,  -5,  20,
+	-20,  -40,  -5,  -5,  -5,  -5, -40, -20,
+	120,  -20,  20,   5,  20,  20, -20, 120,
+};
+
+/**
+ * コンストラクタ
+ */
+reversi::MoveThinkingCpu3::MoveThinkingCpu3() {
+}
+
+/**
+ * デストラクタ
+ */
+reversi::MoveThinkingCpu3::~MoveThinkingCpu3() {
+}
+
+/**
+ * 思考初期化
+ * 手番が来たときに一度だけ呼ばれる
+ * @param  board    盤情報
+ * @param  turn     手番
+ * @return          trueなら思考が完了
+ */
+void reversi::MoveThinkingCpu3::InitializeMoveThinking(const reversi::Board& board, reversi::ReversiConstant::TURN turn) {
+	// 空の位置を探す
+	reversi::ReversiConstant::EMPTY_POSITION emptyPosition;
+	reversiMove.FindEmptyPosition(board, emptyPosition);
+	// 打てる位置を探す
+	reversiMove.FindPutEnablePosition(board, emptyPosition, turn);
+}
+
+/**
+ * 思考
+ * @param  board    盤情報
+ * @param  move     着手情報
+ * @param  turn     手番
+ * @return          trueなら思考が完了
+ */
+bool reversi::MoveThinkingCpu3::MoveThinking(const reversi::Board& board, reversi::MoveInfo& move, reversi::ReversiConstant::TURN turn) {
+	return false;
+}
+
+reversi::ReversiConstant::BOARD_INFO reversi::MoveThinkingCpu3::GetTurnToStone(reversi::ReversiConstant::TURN turn) {
+	if (turn == reversi::ReversiConstant::TURN::TURN_BLACK) {
+		return reversi::ReversiConstant::BOARD_INFO::BLACK;
+	} else {
+		return reversi::ReversiConstant::BOARD_INFO::WHITE;
+	}
+}
---
+++ Reversi/reversi/logic/player/MoveThinkingCpu3.h
@@ -0,0 +1,65 @@
+﻿#ifndef REVERSI_LOGIC_PLAYER_MOVETHINKINGCPU3_H_
+#define REVERSI_LOGIC_PLAYER_MOVETHINKINGCPU3_H_
+
+#include "IMoveThinking.h"
+#include "../base/ReversiConstant.h"
+#include "../base/MoveInfo.h"
+#include "../base/Move.h"
+
+// 前方宣言
+namespace reversi {
+class Board;
+}
+
+namespace reversi {
+
+// プレイヤー(CPU)思考クラス LEVEL3
+// 静的評価関数を使用する
+class MoveThinkingCpu3 : public IMoveThinking {
+private:
+	// 静的評価位置
+	// 石が置いてある場所によって評価をつける(ここの位置はいい、悪いなど)
+	static const int STATIC_EVALUATION_POSITIONS[reversi::ReversiConstant::POSITION_SIZE];
+	// 参考元
+	static const int DEFAULT_STATIC_EVALUATION_POSITIONS[reversi::ReversiConstant::POSITION_SIZE];
+public:
+	/**
+	 * コンストラクタ
+	 */
+	MoveThinkingCpu3();
+
+	/**
+	 * デストラクタ
+	 */
+	virtual ~MoveThinkingCpu3();
+
+	/**
+	 * 思考初期化
+	 * 手番が来たときに一度だけ呼ばれる
+	 * @param  board    盤情報
+	 * @param  turn     手番
+	 * @return          trueなら思考が完了
+	 */
+	virtual void InitializeMoveThinking(const reversi::Board& board, reversi::ReversiConstant::TURN turn);
+
+	/**
+	 * 思考
+	 * @param  board    盤情報
+	 * @param  move     着手情報
+	 * @param  turn     手番
+	 * @return          trueなら思考が完了
+	 */
+	bool MoveThinking(const reversi::Board& board, reversi::MoveInfo& move, reversi::ReversiConstant::TURN turn);
+
+private:
+
+	reversi::ReversiConstant::BOARD_INFO GetTurnToStone(reversi::ReversiConstant::TURN turn);
+
+private:
+	reversi::Move reversiMove;
+
+};
+
+}
+
+#endif  // REVERSI_LOGIC_PLAYER_MOVETHINKINGCPU3_H_
