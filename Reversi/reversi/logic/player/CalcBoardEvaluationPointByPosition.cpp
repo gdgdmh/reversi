0fcf6506869269f40327582af0a9c2123eba4c93
gdgdkuro <gdgdkuro@gmail.com>
2020-04-10 22:34:15 +0900

    評価関数計算インターフェースのコードを仮追加

---
+++ Reversi/reversi/logic/player/CalcBoardEvaluationPointByPosition.cpp
@@ -0,0 +1,35 @@
+﻿#include "CalcBoardEvaluationPointByPosition.h"
+#include "../../util/Assert.h"
+
+// 定数定義
+// 静的評価(位置によるポイント)
+const int reversi::CalcBoardEvaluationPointByPosition::STATIC_EVALUATION_POINTS[reversi::ReversiConstant::POSITION_SIZE] = {
+	60,  -30,  20,   5,   5,  20, -30,  60,
+	-30,  -40,  -5,  -5,  -5,  -5, -40, -30,
+	20,   -5,  20,   3,   3,  20,  -5,  20,
+	5,   -5,   3,   3,   3,   3,  -5,   5,
+	5,   -5,   3,   3,   3,   3,  -5,   5,
+	20,   -5,  20,   3,   3,  20,  -5,  20,
+	-30,  -40,  -5,  -5,  -5,  -5, -40, -30,
+	60,  -30,  20,   5,  20,  20, -30,  60,
+};
+
+/**
+ * コンストラクタ
+ */
+reversi::CalcBoardEvaluationPointByPosition::CalcBoardEvaluationPointByPosition() {
+}
+
+/**
+ * デストラクタ
+ */
+reversi::CalcBoardEvaluationPointByPosition::~CalcBoardEvaluationPointByPosition() {
+}
+
+/**
+ * 盤評価値を計算する
+ * @param board 盤
+ * @param turn  どの手番を評価するか
+ */
+void reversi::CalcBoardEvaluationPointByPosition::CalcBoardEvaluationPoint(const reversi::Board& board, reversi::ReversiConstant::TURN turn) {
+}
---
+++ Reversi/reversi/logic/player/ICalcBoardEvaluationPoint.h
@@ -0,0 +1,28 @@
+﻿#ifndef REVERSI_LOGIC_PLAYER_ICALCBOARDEVALUATIONPOINT_H_
+#define REVERSI_LOGIC_PLAYER_ICALCBOARDEVALUATIONPOINT_H_
+
+#include "../base/ReversiConstant.h"
+#include "../base/Board.h"
+
+namespace reversi {
+
+// 盤評価値計算インターフェース
+class ICalcBoardEvaluationPoint {
+public:
+	/**
+	 * デストラクタ
+	 */
+	virtual ~ICalcBoardEvaluationPoint() {
+	}
+
+	/**
+	 * 盤評価値を計算する
+	 * @param board 盤
+	 * @param turn  どの手番を評価するか
+	 */
+	virtual void CalcBoardEvaluationPoint(const reversi::Board& board, reversi::ReversiConstant::TURN turn) = 0;
+};
+
+}
+
+#endif  // REVERSI_LOGIC_PLAYER_ICALCBOARDEVALUATIONPOINT_H_
