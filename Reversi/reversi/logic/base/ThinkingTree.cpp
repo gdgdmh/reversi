adb3c8701042cafaff86a17d9fa9753bd10fa15f
gdgdkuro <gdgdkuro@gmail.com>
2020-04-10 16:23:17 +0900

    思考ツリーコードを仮追加

---
+++ Reversi/reversi/logic/base/ThinkingTree.cpp
@@ -0,0 +1,14 @@
+﻿#include "ThinkingTree.h"
+#include "../../util/Assert.h"
+
+/**
+ * コンストラクタ
+ */
+reversi::ThinkingTree::ThinkingTree() {
+}
+
+/**
+ * デストラクタ
+ */
+reversi::ThinkingTree::~ThinkingTree() {
+}
---
+++ Reversi/reversi/logic/base/ThinkingTree.h
@@ -0,0 +1,33 @@
+﻿#ifndef REVERSI_LOGIC_BASE_THINKINGTREE_H_
+#define REVERSI_LOGIC_BASE_THINKINGTREE_H_
+
+//#include "ReversiConstant.h"
+//#include "ReverseInfo.h"
+
+namespace reversi {
+
+// 思考ツリークラス
+class ThinkingTree {
+public:
+
+	/**
+	 * コンストラクタ
+	 */
+	ThinkingTree();
+
+	/**
+	 * デストラクタ
+	 */
+	virtual ~ThinkingTree();
+
+private:
+	// リバーシクラス
+	// リスト
+	// 手番
+	// 評価値
+
+};
+
+}
+
+#endif  // REVERSI_LOGIC_BASE_THINKINGTREE_H_
