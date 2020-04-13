76d350c723f879a5a4dc47ab8318c61c8856d713
gdgdkuro <gdgdkuro@gmail.com>
2020-04-13 11:21:37 +0900

    ReversiConstantのコメントを追加

--- Reversi/reversi/logic/base/ReversiConstant.cpp
+++ Reversi/reversi/logic/base/ReversiConstant.cpp
@@ -12,7 +12,7 @@ const reversi::ReversiConstant::POSITION reversi::ReversiConstant::POSITIONS[rev
 	reversi::ReversiConstant::POSITION::A7, reversi::ReversiConstant::POSITION::B7, reversi::ReversiConstant::POSITION::C7, reversi::ReversiConstant::POSITION::D7, reversi::ReversiConstant::POSITION::E7, reversi::ReversiConstant::POSITION::F7, reversi::ReversiConstant::POSITION::G7, reversi::ReversiConstant::POSITION::H7,
 	reversi::ReversiConstant::POSITION::A8, reversi::ReversiConstant::POSITION::B8, reversi::ReversiConstant::POSITION::C8, reversi::ReversiConstant::POSITION::D8, reversi::ReversiConstant::POSITION::E8, reversi::ReversiConstant::POSITION::F8, reversi::ReversiConstant::POSITION::G8, reversi::ReversiConstant::POSITION::H8
 };
-
+// 横方向の座標
 const reversi::ReversiConstant::POSITION reversi::ReversiConstant::POSITION_HORIZONTALS[reversi::ReversiConstant::BOARD_SIZE_Y][reversi::ReversiConstant::BOARD_SIZE_X] = {
 	{ reversi::ReversiConstant::POSITION::A1, reversi::ReversiConstant::POSITION::B1, reversi::ReversiConstant::POSITION::C1, reversi::ReversiConstant::POSITION::D1, reversi::ReversiConstant::POSITION::E1, reversi::ReversiConstant::POSITION::F1, reversi::ReversiConstant::POSITION::G1, reversi::ReversiConstant::POSITION::H1 },
 	{ reversi::ReversiConstant::POSITION::A2, reversi::ReversiConstant::POSITION::B2, reversi::ReversiConstant::POSITION::C2, reversi::ReversiConstant::POSITION::D2, reversi::ReversiConstant::POSITION::E2, reversi::ReversiConstant::POSITION::F2, reversi::ReversiConstant::POSITION::G2, reversi::ReversiConstant::POSITION::H2 },
@@ -22,9 +22,8 @@ const reversi::ReversiConstant::POSITION reversi::ReversiConstant::POSITION_HORI
 	{ reversi::ReversiConstant::POSITION::A6, reversi::ReversiConstant::POSITION::B6, reversi::ReversiConstant::POSITION::C6, reversi::ReversiConstant::POSITION::D6, reversi::ReversiConstant::POSITION::E6, reversi::ReversiConstant::POSITION::F6, reversi::ReversiConstant::POSITION::G6, reversi::ReversiConstant::POSITION::H6 },
 	{ reversi::ReversiConstant::POSITION::A7, reversi::ReversiConstant::POSITION::B7, reversi::ReversiConstant::POSITION::C7, reversi::ReversiConstant::POSITION::D7, reversi::ReversiConstant::POSITION::E7, reversi::ReversiConstant::POSITION::F7, reversi::ReversiConstant::POSITION::G7, reversi::ReversiConstant::POSITION::H7 },
 	{ reversi::ReversiConstant::POSITION::A8, reversi::ReversiConstant::POSITION::B8, reversi::ReversiConstant::POSITION::C8, reversi::ReversiConstant::POSITION::D8, reversi::ReversiConstant::POSITION::E8, reversi::ReversiConstant::POSITION::F8, reversi::ReversiConstant::POSITION::G8, reversi::ReversiConstant::POSITION::H8 }
-
 };
-
+// 位置の文字列テーブル(POSITIONSのindexに対応)
 const std::string reversi::ReversiConstant::POSITION_STRINGS[ReversiConstant::POSITION_SIZE] = {
 	"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1",
 	"A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
@@ -43,7 +42,6 @@ const std::string reversi::ReversiConstant::POSITION_STRINGS[ReversiConstant::PO
  * @return                trueなら変換できた
  */
 bool reversi::ReversiConstant::GetStringToPosition(const std::string& positionString, reversi::ReversiConstant::POSITION& position) {
-
 	for (int i = 0; i < reversi::ReversiConstant::POSITION_SIZE; ++i) {
 		reversi::Assert::AssertArrayRange(i, reversi::ReversiConstant::POSITION_SIZE, "ReversiConstant::GetStringToPosition index over POSITION_STRINGS");
 		if (positionString == POSITION_STRINGS[i]) {
@@ -62,7 +60,6 @@ bool reversi::ReversiConstant::GetStringToPosition(const std::string& positionSt
  * @return                trueなら変換できた
  */
 bool reversi::ReversiConstant::GetPositionToString(const reversi::ReversiConstant::POSITION& position, std::string& positionString) {
-
 	// POSITONSとPOSITION_STRINGSのindexは対応している
 	for (int i = 0; i < reversi::ReversiConstant::POSITION_SIZE; ++i) {
 		reversi::Assert::AssertArrayRange(i, reversi::ReversiConstant::POSITION_SIZE, "ReversiConstant::GetPositionToString index over");
@@ -91,6 +88,11 @@ int reversi::ReversiConstant::GetPositionToIndex(const reversi::ReversiConstant:
 	return -1;
 }
 
+/**
+ * 手番の切り替えをする
+ * @param  turn 切り替えする手番
+ * @return      切り替えされた手番
+ */
 reversi::ReversiConstant::TURN reversi::ReversiConstant::InvertTurn(const reversi::ReversiConstant::TURN& turn) {
 	if (turn == reversi::ReversiConstant::TURN::TURN_BLACK) {
 		return reversi::ReversiConstant::TURN::TURN_WHITE;
--- Reversi/reversi/logic/base/ReversiConstant.h
+++ Reversi/reversi/logic/base/ReversiConstant.h
@@ -9,8 +9,6 @@ namespace reversi {
 // リバーシ定数クラス
 class ReversiConstant {
 public:
-
-	// 定数定義
 	// 盤の情報
 	enum class BOARD_INFO {
 		NONE,       // 石が置かれていない
@@ -19,7 +17,7 @@ public:
 		INVALID,    // 無効マス
 	};
 
-	//　盤の座標
+	//　盤の座標(有効座標のみ)
 	enum class POSITION {
 		A1 = 11, B1 = 12, C1 = 13, D1 = 14, E1 = 15, F1 = 16, G1 = 17, H1 = 18,
 		A2 = 21, B2 = 22, C2 = 23, D2 = 24, E2 = 25, F2 = 26, G2 = 27, H2 = 28,
@@ -30,44 +28,38 @@ public:
 		A7 = 71, B7 = 72, C7 = 73, D7 = 74, E7 = 75, F7 = 76, G7 = 77, H7 = 78,
 		A8 = 81, B8 = 82, C8 = 83, D8 = 84, E8 = 85, F8 = 86, G8 = 87, H8 = 88
 	};
-
 	// 手番
 	enum class TURN {
 		TURN_BLACK, // 黒の手番
 		TURN_WHITE, // 白の手番
 	};
-
 	// 着手情報
 	typedef struct {
 		POSITION position;  // 位置
 		BOARD_INFO info;    // 着手する石
 		TURN turn;          // 手番
 	} MOVE_INFO;
-
 	// 石が置かれていない場所データ
 	typedef struct {
 		std::vector<reversi::ReversiConstant::POSITION> position;
 	} EMPTY_POSITION;
 
-	static const int BOARD_DATA_SIZE_X = 10; // 盤のデータサイズX
-	static const int BOARD_DATA_SIZE_Y = 10; // 盤のデータサイズY
+	static const int BOARD_DATA_SIZE_X = 10;    // 盤のデータサイズX
+	static const int BOARD_DATA_SIZE_Y = 10;    // 盤のデータサイズY
 	static const int BOARD_SIZE = ReversiConstant::BOARD_DATA_SIZE_Y * ReversiConstant::BOARD_DATA_SIZE_X;     // 盤の大きさ(1次元配列) 8x8の盤だが斜め判定を考慮して全体を無効マスで囲う用にしている
-	static const int BOARD_SIZE_X = 8;      // 盤の大きさX(Xマスの数)
-	static const int BOARD_SIZE_Y = 8;      // 盤の大きさY(Yマスの数)
-	static const int POSITION_SIZE = 64;    // 座標の最大数
+	static const int BOARD_SIZE_X = 8;          // 盤の大きさX(Xマスの数)
+	static const int BOARD_SIZE_Y = 8;          // 盤の大きさY(Yマスの数)
+	static const int POSITION_SIZE = 64;        // 座標の最大数
 
 	static const int ONE_MOVE_MAX_REVERSE_COUNT = 18; // 1回の着手でひっくり返る最大数
 
-	static const ReversiConstant::POSITION POSITIONS[ReversiConstant::POSITION_SIZE]; // 全ての座標(通常使用するもののみ、番兵は含まない)
+	static const ReversiConstant::POSITION POSITIONS[ReversiConstant::POSITION_SIZE];   // 全ての座標(通常使用するもののみ、番兵は含まない)
 	static const ReversiConstant::POSITION POSITION_HORIZONTALS[ReversiConstant::BOARD_SIZE_Y][ReversiConstant::BOARD_SIZE_X]; // 横方向の座標
-	static const std::string POSITION_STRINGS[ReversiConstant::POSITION_SIZE]; // 位置の文字列テーブル
-
+	static const std::string POSITION_STRINGS[ReversiConstant::POSITION_SIZE];          // 位置の文字列テーブル(POSITIONSのindexに対応)
 public:
 
-	// 座標の取得(参照回数が多くなる可能性があるのでインライン)
 	// 基本的にはPOSITIONの座標に対して使用すること
 	// INVALIDの座標に対して使用すると座標外を参照する可能性がある
-
 	/**
 	 * 盤の指定座標の上の座標を取得する
 	 * @param  position 指定座標
@@ -163,6 +155,11 @@ public:
 	 */
 	static int GetPositionToIndex(const reversi::ReversiConstant::POSITION& position);
 
+	/**
+	 * 手番の切り替えをする
+	 * @param  turn 切り替えする手番
+	 * @return      切り替えされた手番
+	 */
 	static reversi::ReversiConstant::TURN InvertTurn(const reversi::ReversiConstant::TURN& turn);
 
 };
