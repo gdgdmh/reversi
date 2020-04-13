0446b522cf1e5e3b3b451f3bbcd2ea123b9b4c27
gdgdkuro <gdgdkuro@gmail.com>
2020-04-13 13:20:29 +0900

    IMoveThinkingで余計なコードを削除

--- Reversi/reversi/logic/player/IMoveThinking.h
+++ Reversi/reversi/logic/player/IMoveThinking.h
@@ -38,26 +38,6 @@ public:
 	 * @return           trueなら思考が完了
 	 */
 	virtual bool MoveThinking(const reversi::Reversi& reversi, const reversi::Move& moveCache, const reversi::Board& board, reversi::MoveInfo& move, reversi::ReversiConstant::TURN turn) = 0;
-
-	/**
-	 * 思考初期化
-	 * 手番が来たときに一度だけ呼ばれる
-	 * @param  board    盤情報
-	 * @param  turn     手番
-	 * @return          trueなら思考が完了
-	 */
-	//virtual void InitializeMoveThinking(const reversi::Board& board, reversi::ReversiConstant::TURN turn) = 0;
-
-	/**
-	 * 思考
-	 * trueを返さない限りは呼ばれ続ける
-	 * @param  board    盤情報
-	 * @param  move     着手情報
-	 * @param  turn     手番
-	 * @return          trueなら思考が完了
-	 */
-	//virtual bool MoveThinking(const reversi::Board& board, reversi::MoveInfo& move, reversi::ReversiConstant::TURN turn) = 0;
-
 };
 
 }
