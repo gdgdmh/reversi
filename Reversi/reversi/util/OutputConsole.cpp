df3cfe5cdf346a0ba166dc356dad23ab400cd297
gdgdkuro <gdgdkuro@gmail.com>
2020-04-13 17:38:26 +0900

    タブと改行を調整

--- Reversi/reversi/util/OutputConsole.cpp
+++ Reversi/reversi/util/OutputConsole.cpp
@@ -5,26 +5,24 @@
  * コンストラクタ
  */
 reversi::OutputConsole::OutputConsole() {
-
 }
 
 /**
  * デストラクタ
  */
 reversi::OutputConsole::~OutputConsole() {
-
 }
 
 /**
  * 文字列を出力する
  */
 void reversi::OutputConsole::Print(std::string string) {
-    std::cout << string;
+	std::cout << string;
 }
 
 /**
  * 改行付き文字列を出力する
  */
 void reversi::OutputConsole::PrintLine(std::string string) {
-    std::cout << string << std::endl;
+	std::cout << string << std::endl;
 }
